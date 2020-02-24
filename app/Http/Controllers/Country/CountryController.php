8a15a432069981438fc6f2b56b9633e34a5ae988
ddh <imam2889@gmail.com>
2020-02-24 17:40:22 +0600

    Country Find by ID

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -11,4 +11,7 @@ class CountryController extends Controller
    public function country(){
      return response()->json(CountryModel::get(),200);
    }
+    public function countryByID($id){
+        return response()->json(CountryModel::find($id),200);
+    }
 }
